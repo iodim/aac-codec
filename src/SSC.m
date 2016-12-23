4c247b46807a881cf361ef8294340ba4138a13ff
iodim <ioanndima@gmail.com>
2016-12-23 02:03:05 +0200

    Fixed SSC

--- src/SSC.m
+++ src/SSC.m
@@ -1,17 +1,20 @@
 function frameType = SSC(frameT, nextFrameT, prevFrameType)
 %SSC Implements Sequence Segmentation Control step
 %   frameType = SSC(frameT, nextFrameT, prevFrameType)
-%   frameType: Can take one of the following values:
-%       -'OLS' for ONLY_LONG_SEQUENCE (1)
-%       -'LSS' for LONG_START_SEQUENCE (2)
-%       -'ESH' for EIGHT_SHORT_SEQUENCE (3)
-%       -'LPS' for LONG_STOP_SEQUENCE (4)
-%   frameT: Frame i in the time domain. Contains 2 audio channels. 2048x2
-%       matrix.
-%   nextFrameT: Next frame, with index i+1. Used for window selection.
-%       2048x2 matrix.
-%   prevFrameType: Type that was chosen for the previous frame, with index
-%       i-1.
+%   Returns the following values:
+%    - frameType: Can take one of the following values:
+%       - 'OLS' for ONLY_LONG_SEQUENCE (1)
+%       - 'LSS' for LONG_START_SEQUENCE (2)
+%       - 'ESH' for EIGHT_SHORT_SEQUENCE (3)
+%       - 'LPS' for LONG_STOP_SEQUENCE (4)
+%   Accepts the following arguments:
+%    - frameT: Frame i in the time domain. Contains 2 audio channels.
+%    Matrix of size 2048x2.
+%    - nextFrameT: Next frame, with index i+1. Used for window selection.
+%    Matrix of size 2048x2.
+%    - prevFrameType: Type that was chosen for the previous frame, with
+%   index i-1. Can also take the value NUL (0).
+    NUL = 0;
     OLS = 1;
     LSS = 2;
     ESH = 3;
@@ -22,22 +25,26 @@ function frameType = SSC(frameT, nextFrameT, prevFrameType)
         s = sum(reshape(filtered(577:1600), [128, 8]).^2, 1)';
         ds = zeros(8, 1);
         for l = 1:8
-            ds(l) = l*s(l)/sum(s(1:l));
+            ds(l) = l*s(l)/sum(s(1:l-1));
         end
         isNextFrameESH = any((s > 1e-3) & (ds > 10));
         switch prevFrameType
             case OLS
-                if (isNextFrameESH) candidate(ch) = LSS;
+                if (isNextFrameESH), candidate(ch) = LSS;
                 else candidate(ch) = OLS;
                 end
             case ESH
-                if (isNextFrameESH) candidate(ch) = ESH;
+                if (isNextFrameESH), candidate(ch) = ESH;
                 else candidate(ch) = LPS;
                 end
             case LSS
                 candidate(ch) = ESH;
             case LPS
                 candidate(ch) = OLS;
+            case NUL
+                if (isNextFrameESH), candidate(ch) = ESH;
+                else candidate(ch) = OLS;
+                end
         end
     end
     if any(candidate == ESH)
