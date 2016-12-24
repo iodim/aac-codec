ce5bfdec2b2da3a49e237d158aa8ca414f4311ab
iodim <ioanndima@gmail.com>
2016-12-24 17:36:08 +0200

    deleted leftovers

--- src/L1demo.m
+++
@@ -1,6 +0,0 @@
-clc; clear; close all;
-
-demoAAC1('LicorDeCalandraca.wav', 'LicorDeCalandraca_2.wav')
-[y, fs] = audioread('LicorDeCalandraca_2.wav');
-player = audioplayer(y, fs);
-% player.play();
