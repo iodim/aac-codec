54ef438c386294797b94a87c9476adf44cf4dfb1
iodim <ioanndima@gmail.com>
2016-12-24 19:22:48 +0200

    changed demo

--- src/demo.m
+++ src/demo.m
@@ -2,9 +2,10 @@ clc; clear; close all;
 
 addpath('./Level 1/')
 addpath('./Level 2/')
+addpath('./Level 3/')
 
-demoAAC1('LicorDeCalandraca.wav', 'LicorDeCalandraca_2.wav')
-demoAAC2('LicorDeCalandraca.wav', 'LicorDeCalandraca_2.wav')
+demoAAC1('LicorDeCalandraca.wav', 'LicorDeCalandraca_L1.wav')
+demoAAC2('LicorDeCalandraca.wav', 'LicorDeCalandraca_L2.wav')
 
 %[y, fs] = audioread('LicorDeCalandraca_2.wav');
 %player = audioplayer(y, fs);
